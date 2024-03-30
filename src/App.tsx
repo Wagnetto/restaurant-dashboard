53d3a22d1f38fa8ceecad5c322f7682b9b311a26
Wagnetto <wagnetto@protonmail.com>
2024-03-30 18:05:44 -0300

    style/Add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './global.css'
-import { Button } from "@/components/ui/button"
 
+import { Button } from '@/components/ui/button'
 
 export function App() {
-
   return (
-    <div className="flex justify-center h-screen items-center">
+    <div className="flex h-screen items-center justify-center">
       <Button>Enviar</Button>
     </div>
   )
 }
-
