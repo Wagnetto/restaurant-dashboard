a47cbe222f54a6d1fcf80c185661d3b51e2242f7
Wagnetto <wagnetto@protonmail.com>
2024-09-27 14:46:16 -0300

    workaround(API-connection): Avoid TS bug over zod

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod'
-
+//@ts-ignore
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
   VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
 })
 
-export const env = envSchema.parse(import.meta.env)
+export const env = import.meta.env
