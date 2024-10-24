8722c459a9bdde78fefc0f8d47fcd9598af208c3
Wagnetto <wagnetto@protonmail.com>
2024-10-24 12:40:47 -0300

    bugfix(API-Connection): Fix environment variables and axios delay

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod'
-// @ts-ignore
+
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
   VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
 })
 
-export const env = import.meta.env
+const parsedEnv = envSchema.parse(import.meta.env);
+
+export const env = {
+  ...parsedEnv,
+  VITE_API_URL: parsedEnv.VITE_API_URL,
+  VITE_ENABLE_API_DELAY: parsedEnv.VITE_ENABLE_API_DELAY,
+};
