5f10cde8900a4b1d54bad4e1e22802366c90377e
Wagnetto <wagnetto@protonmail.com>
2024-10-29 14:59:19 -0300

    feature(test): Add Provider to NavLink test

---
+++ src/components/nav-link.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import { NavLink } from "./nav-link";
+import { MemoryRouter } from "react-router-dom";
+
+describe("NavLink", () => {
+  it("should highlight the nav link when match the current page link", () => {
+    render(
+      <>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/home">Home</NavLink>
+      </>,
+      {
+        wrapper: ({ children }) => {
+          return (
+            <MemoryRouter initialEntries={["/about"]}>{children}</MemoryRouter>
+          );
+        },
+      },
+    );
+
+    expect(screen.getByText(/About/i).dataset.current).toEqual("true");
+    expect(screen.getByText(/Home/i).dataset.current).toEqual("false");
+  });
+});
