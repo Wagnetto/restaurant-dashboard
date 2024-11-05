e9bd100e5e4efb7f898820ff8d5cd52560a54378
Wagnetto <wagnetto@protonmail.com>
2024-11-05 19:09:53 -0300

    lint: fix prettier plugin

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,3 +1,10 @@
 module.exports = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
   plugins: ['prettier-plugin-tailwindcss'],
 }
