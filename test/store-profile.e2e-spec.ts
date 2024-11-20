140ef04cf09e132d0198d3d152562f61148e06bb
Wagner Souto <wagnetto@protonmail.com>
2024-11-20 20:30:34 -0300

    feature(test): Add update-profile E2E flow

---
+++ test/store-profile.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { expect, test } from '@playwright/test'
+
+test('update profile successfully', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Burger House' }).click()
+  page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+  page.getByLabel('Nome').fill('Vegan Burger')
+  page.getByLabel('Descrição').fill('Vegan Description')
+  page.getByRole('button', { name: 'Salvar' }).click()
+
+  const successToast = page.getByText('Perfil atualizado com sucesso!')
+  await expect(successToast).toBeVisible()
+
+  page.getByRole('button', { name: 'Close' }).click()
+
+  await expect(page.getByRole('button', { name: 'Vegan Burger' })).toBeVisible()
+})
+
+test('update profile with error', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Burger House' }).click()
+  page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+  page.getByLabel('Nome').fill('Wrong Name')
+  page.getByLabel('Descrição').fill('Wrong Description')
+  page.getByRole('button', { name: 'Salvar' }).click()
+
+  const errorToast = page.getByText(
+    'Falha ao atualizar o perfil, tente novamente.',
+  )
+  await expect(errorToast).toBeVisible()
+})
